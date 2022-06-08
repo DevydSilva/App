7cead613ba47f52a1c31946594f28c23eb001857
devydk <100238297+projIARafael@users.noreply.github.com>
2022-06-07 22:05:55 -0300

    commit

---
+++ App/styles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+       background-color: #63C2D1;
+       flex: 1;
+       justify-content: center;
+       align-items: center; 
+`;
+
+export const Text = styled.Text`
+      font-size: 18px;
+      color: #FFF;
+`;
